8e241617056e9b6295945e58cb33a0871f3c06dc
ksenia <kse@mover24.ru>
2020-08-27 14:52:46 +0100

    first

---
+++ app/src/main/java/ru/ksuta/pokemonowltest/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.ksuta.pokemonowltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
